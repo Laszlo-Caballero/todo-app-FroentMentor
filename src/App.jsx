35afb0e49d9b9f003785f7a4707b97846934e08a
Laszlo-Caballero <laszlocaballero@gmail.com>
2023-12-21 11:47:37 -0500

    fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Todos from "./components/Todos";
 function App() {
   return (
     <>
-      <div className="min-h-screen w-screen mobile:dark:bg-DarkMobile md:dark:bg-DarkDesktop bg-no-repeat dark:bg-DarkBlue mobile:bg-LightMobile md:bg-LightDesktop bg-LightGray flex justify-center bg-scroll">
+      <div className="min-h-screen w-screen mobile:dark:bg-DarkMobile md:dark:bg-DarkDesktop bg-no-repeat dark:bg-DarkBlue mobile:bg-LightMobile md:bg-LightDesktop bg-LightGray flex justify-center ">
         <main className="mobile:w-[90%] md:w-[38%] min-h-[700px] flex flex-col md:mt-20 mobile:mx-[20px] mobile:mt-16">
           <Header />
-          <section className="dark:text-LightGrayishBlue overflow-hidden text-DarkGrayishWhite">
+          <section className="dark:text-LightGrayishBlue text-DarkGrayishWhite">
             <Todos />
           </section>
         </main>
